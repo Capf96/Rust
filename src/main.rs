5d9e95d43419344ef4449454c461efbfee3f2ac8
Maria B <mabraca18@gmail.com>
2018-03-21 23:54:57 -0400

    Recorrer directorios

--- src/main.rs
+++ src/main.rs
@@ -19,24 +19,9 @@ fn recorrer_Path(ext: &str){
 		let p= paths.unwrap();
 		let file_name = p.file_name();
 		let file_name_as_str = file_name.to_str().unwrap();
-		let k = Path::new(file_name);
 		if file_name_as_str.contains(ext) == true {
 			println!("{:?}", p.path().display());
 		};
-		
- 
-
-		/*let c = match k.extension().and_then(OsStr::to_str){
-			Some(x) => if x == ext { Some(path.display()) } else { None },
-			None => None,
-		};
-		if c != None { 
-			match c {
-				Some(x) => println!("{}", x),
-				_ => println!("Esto nunca se va a imprimir"),
-			}
-		}*/
-	
 		
 	}
 }
